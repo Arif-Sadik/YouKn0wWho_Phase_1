2904ca7a45c1d9ee4799ecf683394307ea5e7f56
Md. Arif Sadik Molla <arifsadik244@gmail.com>
2025-03-22 17:20:53 +0600

    Create Mirror_array.cpp

---
+++ Mirror_array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[n + 1][m + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = m; j >= 1; j--)
+        {
+            cout << a[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
