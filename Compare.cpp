38da0f3dc84e4d80e135afe6a1dec5f40e5e47d6
Md. Arif Sadik Molla <arifsadik244@gmail.com>
2025-03-26 00:06:18 +0600

    Create Compare.cpp

---
+++ Compare.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>                          /// cpp + ctrl+j
+#define ll long long
+#define dubb long double
+#define pi acos(-1)
+using namespace std;
+const ll N=1000;
+char s[N], t[N];
+
+int main()
+{
+    cin >> s >> t;
+    int length_s = strlen(s);
+    int length_t = strlen(t);
+    if (strcmp(s,t)<0)
+    {
+        cout<<s;
+    }
+    else cout<<t;
+
+
+
+
+
+
+    return 0;
+}
