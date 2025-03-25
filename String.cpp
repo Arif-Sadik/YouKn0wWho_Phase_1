e5d7325ee8721b6650baa64fb3cd0046b0a1e00d
Md. Arif Sadik Molla <arifsadik244@gmail.com>
2025-03-26 00:22:43 +0600

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -1,26 +1,21 @@
-#include<bits/stdc++.h>                          /// cpp + ctrl+j
-#define ll long long
-#define dubb long double
-#define pi acos(-1)
+#include <bits/stdc++.h>
 using namespace std;
-const ll N=1000;
-char s[N], t[N];
 
-int main()
-{
+int main() {
+    char s[1000], t[1000];
     cin >> s >> t;
+
     int length_s = strlen(s);
     int length_t = strlen(t);
-    char p[1000];
-    for (int i=0;s[i]!='\0';i++)
-    {
-        p[i]=s[i];
-    }
-    cout<<length_s<<" "<<length_t<<endl;
-    cout<<strcat(p,t)<<endl;
-    swap(s[0],t[0]);
-    cout<<s<<" "<<t<<endl;
 
+    cout << length_s << " " << length_t << endl;
+
+    char concat_s[2000];  
+    strcpy(concat_s, s);  
+    cout << strcat(concat_s, t) << endl;
+
+    swap(s[0], t[0]);
+    cout << s << " " << t << endl;
 
     return 0;
 }
