7fee98003762b0e8c5ded687212ebeca093403cd
Md. Arif Sadik Molla <arifsadik244@gmail.com>
2025-03-26 00:29:07 +0600

    Update Count.cpp

--- Count.cpp
+++ Count.cpp
@@ -1,24 +1,15 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define dubb long double
-#define pi acos(-1)
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
+    long long c = 0;
+    string s;
+    cin >> s;
 
-    ll c=0;
-    char s[1000];
-
-        cin>>s;
-        for (int i=0;i<strlen(s);i++)
-        {
-            c+=s[i]-'0';
-        }
-        cout<<c;
-
-
-
-
-return 0;
+    for (int i = 0; i < s.length(); i++) {
+        c += s[i] - '0';  // Convert character to integer and add to sum
+    }
+    
+    cout << c << endl;
+    return 0;
 }
